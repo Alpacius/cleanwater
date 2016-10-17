39426affa7cdd2810c7051b91f3422161ec3b0eb
Akvelog <sceneviper@hotmail.com>
2016-10-18 01:03:59 +0900

    Error fix.

--- common/cp_queue.h
+++ common/cp_queue.h
@@ -6,43 +6,32 @@
 #include    "./util_list.h"
 
 
-static uint8_t index_mapping_[] = {
-    [0]            = 0,
-    [(uint8_t) -1] = 1
-};
-
 struct cp_queue {
-    uint8_t index_producer;
+    uint8_t index_producer, producer_busy;
     struct link_index buffers[2];
 };
 
-#define producer_index_of(queue_) index_mapping_[__atomic_load_n(&((queue_)->index_producer), __ATOMIC_ACQUIRE)]
-#define consumer_index_of(queue_) (1 - producer_index_of((queue_)))
-#define new_consumer_index_of(queue_) \
-    ({ \
-        __auto_type queue__ = (queue_); \
-        index_mapping_[ \
-            __atomic_fetch_xor(&(queue__->index_producer), (uint8_t) -1, __ATOMIC_ACQ_REL) \
-        ]; \
-    })
-
+// Beware: the consumer may fail to refresh its buffer.
 static inline
 struct link_index *cp_queue_consume(struct cp_queue *queue) {
-    uint8_t index_consumer = consumer_index_of(queue);
-    return
-        list_is_empty(&(queue->buffers[index_consumer])) ?
-            &(queue->buffers[new_consumer_index_of(queue)]) :
-            &(queue->buffers[index_consumer]);
+    uint8_t index_consumer = 1 - __atomic_load_n(&(queue->index_producer), __ATOMIC_ACQUIRE), desired_producer_state = 0;
+    if (list_is_empty(&(queue->buffers[index_consumer]))) {
+        __atomic_compare_exchange_n(&(queue->index_producer), &desired_producer_state, 1 - index_consumer, 0, __ATOMIC_ACQ_REL, __ATOMIC_RELAXED);
+    }
+    return &(queue->buffers[index_consumer]);
 }
 
 static inline
 void cp_queue_produce(struct cp_queue *queue, struct link_index *product) {
-    list_add_tail(product, &(queue->buffers[producer_index_of(queue)]));
+    __atomic_store_n(&(queue->producer_busy), 1, __ATOMIC_RELEASE);
+    list_add_tail(product, &(queue->buffers[__atomic_load_n(&(queue->index_producer), __ATOMIC_ACQUIRE)]));
+    __atomic_store_n(&(queue->producer_busy), 0, __ATOMIC_RELEASE);
 }
 
 static inline
 struct cp_queue *cp_queue_init(struct cp_queue *queue) {
     __atomic_store_n(&(queue->index_producer), 0, __ATOMIC_RELEASE);
+    __atomic_store_n(&(queue->producer_busy), 0,__ATOMIC_RELEASE);
     list_init(&(queue->buffers[0]));
     list_init(&(queue->buffers[1]));
 }
